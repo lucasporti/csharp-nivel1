a22c02e4ca6cb072e964b4d91fccdaa2a22d89dd
Lucas <lucassportillo09@gmail.com>
2023-09-15 11:49:04 -0300

    Ejercicio #1 - Finalizado

---
+++ Semana-4-Ciclos/#1-Ejercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1_Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Hacer un programa para mostrar los números del 1 al 10. No se debe realizar ningún pedido de datos.
+            for(int i = 1; i < 11; i++){
+                Console.WriteLine("Número " + i);
+            }
+        }
+    }
+}
