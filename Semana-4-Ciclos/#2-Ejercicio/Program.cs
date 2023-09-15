d3dacc7dafaec35315090fead5b46b3cbb33252c
Lucas <lucassportillo09@gmail.com>
2023-09-15 11:57:05 -0300

    Ejercicio #2 - Finalizado

---
+++ Semana-4-Ciclos/#2-Ejercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2_Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. Hacer un programa que solicite el ingreso de 10 números y que muestre el mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
+            int n = 0, mayor = 0;
+
+            for(int x = 0; x < 10; x++){
+                Console.WriteLine("Ingrese un número");
+                n = int.Parse(Console.ReadLine());
+                if(n > mayor){
+                    mayor = n;
+                }
+            }
+            Console.WriteLine("El número mayor es: " + mayor);
+        }
+    }
+}
