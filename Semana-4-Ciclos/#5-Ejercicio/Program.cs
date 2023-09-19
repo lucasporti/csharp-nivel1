09307e531024e5ca881626aa3daf8caf92aa27c6
Lucas <lucassportillo09@gmail.com>
2023-09-19 10:53:17 -0300

    Ejercicio #5 - Terminado

---
+++ Semana-4-Ciclos/#5-Ejercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5_Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5. Hacer un programa que solicite 20 números y luego emitir por 
+            // pantalla el máximo de los números pares y el mínimo de los números impares.
+            
+            int mayor = 0, menor = 0;
+            for(int x = 0; x < 20; x++){
+                Console.WriteLine("Ingresá un número");
+                int n = int.Parse(Console.ReadLine());
+                if(x == 0){
+                    mayor = n;
+                    menor = n;
+                }
+                if(n%2 == 0 && n>mayor){
+                    mayor = n;
+                }
+                else if (n < menor){
+                    menor = n;
+                }
+            }
+            Console.WriteLine("Máximo de los mayores: " + mayor);
+            Console.WriteLine("Mínimo de los menores: " + menor);
+        }
+    }
+}
