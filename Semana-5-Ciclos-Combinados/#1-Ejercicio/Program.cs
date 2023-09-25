dfac34fa5ca2027e3e45c8e2b0e040dc081987a3
Lucas <lucassportillo09@gmail.com>
2023-09-25 09:06:40 -0300

    Ejercicio #1 - Finalizado

---
+++ Semana-5-Ciclos-Combinados/#1-Ejercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1_Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para ingresar 10 números. El mismo debe analizar y mostrar por pantalla cuántos de esos números son primos.
+
+            int n, primos = 0, flag = 0;
+
+            for(int x = 0; x < 10; x++)
+            {
+                Console.WriteLine("Ingrese un número");
+                n = int.Parse(Console.ReadLine());
+                
+                for(int i = 1; i <= n; i++){
+                    if(n%i == 0)
+                        flag++;
+                }
+                
+                if(flag == 2)
+                        primos++;
+
+                flag = 0;
+            }
+            Console.WriteLine("En el programa se ingresaron: " + primos + " numeros primos.");
+        }
+    }
+}
